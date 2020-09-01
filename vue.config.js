9fdc1adb426d92477e95d19ff0bff51d5e9ad4cc
Zeki <zekioztorun@gmail.com>
2020-09-01 23:39:34 +0300

    Setup vue project

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pwa: {
+    name: "pomodoro"
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: true
+    }
+  }
+};
